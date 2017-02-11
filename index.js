f1d108a40337cfb1ebd3dac82e8918f5ccc01259
Nikhil Verma <nikhil.verma@corp.badoo.com>
2017-02-11 17:11:39 +0000

    Initial commit with the fixes

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const FakeTimers = require('jest-util').FakeTimers;
+const ModuleMocker = require('jest-mock');
+
+function deepCopy(obj) {
+    const newObj = {};
+    let value;
+    for (const key in obj) {
+        value = obj[key];
+        if (typeof value === 'object' && value !== null) {
+            value = deepCopy(value);
+        }
+        newObj[key] = value;
+    }
+    return newObj;
+}
+
+// Object.assign polyfill with try/catch for each assignment expression
+function assign(target) { // .length of function is 2
+    if (target === null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    const to = Object(target);
+
+    for (let index = 1; index < arguments.length; index++) {
+        const nextSource = arguments[index];
+
+        if (nextSource !== null) { // Skip over if undefined or null
+            for (const nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    try {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                    catch (e) {}
+                }
+            }
+        }
+    }
+    return to;
+}
+
+class NodeDebugEnvironment {
+    constructor(config) {
+
+        // jest doesn't need full global before runScript, but to reduce possible issues we create full copy here
+        this.global = assign({}, global, deepCopy(config.globals));
+
+        this.moduleMocker = new ModuleMocker(global);
+        this.fakeTimers = new FakeTimers(this.global, this.moduleMocker, config);
+    }
+
+    dispose() {
+        if (this.fakeTimers) {
+            this.fakeTimers.dispose();
+        }
+        this.fakeTimers = null;
+    }
+
+    runScript(script) {
+
+        // we cannot set global.console to new value (TypeError: Cannot set property console of #<Object> which has only a getter)
+        const jestCustomConsole = this.global.console;
+
+        if (jestCustomConsole !== null) {
+            // but we can alter properties in any case
+            assign(global.console, jestCustomConsole);
+
+            // delete because later we will apply all jest changes from this.global to global
+            delete this.global.console;
+        }
+
+        assign(global, this.global);
+
+        // it seems Jest hold reference to this.global object and next line doesn't affect, so, we restore console
+        this.global.console = jestCustomConsole;
+
+        // during runInThisContext global will be modified (jasmine will set global.jasmineRequire), so, we must set this.global to global (jest will not modify global anymore, so, it is safe)
+        this.global = global;
+
+        return script.runInThisContext();
+    }
+}
+
+module.exports = NodeDebugEnvironment;
